fdbedccc4279f9bf3218ca5fa3080ebe63cee761
teerus <z83@foxmail.com>
2019-07-12 15:58:41 +0800

    初始化项目

---
+++ dive-in-spring-boot/src/test/java/com/imooc/diveinspringboot/DiveInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.diveinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveInSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
